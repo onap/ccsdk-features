43b4f3fc292fa69361065ca3343e5e37eb41c209
Alexis de Talhouët <adetalhouet89@gmail.com>
2018-09-14 15:36:00 -0400

    Quickfix to missing SQL table - DD
    Change-Id: I20a654a5d7efa2b019adf988c2ee04b75af02bb0
    Issue-ID: CCSDK-572
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- blueprints-processor/adaptors/data-adaptor-provider/src/main/java/org/onap/ccsdk/features/data/adaptor/dao/ConfigPropertyMapDaoImpl.java
+++ blueprints-processor/adaptors/data-adaptor-provider/src/main/java/org/onap/ccsdk/features/data/adaptor/dao/ConfigPropertyMapDaoImpl.java
@@ -60,8 +60,8 @@ public class ConfigPropertyMapDaoImpl implements ConfigPropertyMapDao {
                 configPropertyMap.put(key, value);
             });
             logger.trace("loaded configPropertyMap : ({})", configPropertyMap);
-        } catch (CannotGetJdbcConnectionException e) {
-            // WHAT TO DO?
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
         }
     }
 
