b4d7ca85b122f914fb43a4a3f4300e36dede9e2b
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2019-02-28 17:43:35 +0100

    Replace depreciated MDSAL interfaces
    SDN-R websocketmanager2
    
    Change-Id: I69c12ee993bf8e2740d8f72778d06c89bf758051
    Issue-ID: SDNC-680
    Signed-off-by: Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>

--- sdnr/wt/websocketmanager2/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/websocketmanager2/test/UserScopeTest.java
+++ sdnr/wt/websocketmanager2/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/websocketmanager2/test/UserScopeTest.java
@@ -6,32 +6,41 @@
  * =================================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  * ============LICENSE_END==========================================================================
  ******************************************************************************/
-package org.onap.ccsdk.features.sdnr.wt.websocketmanager2;
+package org.onap.ccsdk.features.sdnr.wt.websocketmanager2.test;
 
-import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import static org.junit.Assert.*;
 
-public abstract class Blueprint implements AutoCloseable {
+import org.json.JSONArray;
+import org.junit.Test;
+import org.onap.ccsdk.features.sdnr.wt.websocketmanager2.utils.UserScopes;
 
-    private RpcProviderService rpcProviderRegistry = null;
+public class UserScopeTest {
 
-    public abstract void init();
+    private static final String SCOPE1 ="scope1";
+    private static final String SCOPE2 ="scope2";
+    private static final String SCOPE3 ="scope3";
+    private static final String SCOPE4 ="scope4";
 
-    public void setRpcProviderRegistry(RpcProviderService rpcProviderRegistry) {
-        this.rpcProviderRegistry = rpcProviderRegistry;
+    @Test
+    public void test() {
+        UserScopes scopes1 = new UserScopes();
+        JSONArray json1=new JSONArray();
+        json1.put(SCOPE1);
+        json1.put(SCOPE2);
+        json1.put(SCOPE3);
+        scopes1.setScopes(json1);
+
+        assertTrue(scopes1.hasScope(SCOPE1));
+        assertFalse(scopes1.hasScope(SCOPE4));
     }
-
-    public RpcProviderService getRpcProviderRegistry() {
-        return this.rpcProviderRegistry;
-    }
-
 
 }
