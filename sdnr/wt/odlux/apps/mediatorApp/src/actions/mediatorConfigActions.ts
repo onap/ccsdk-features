c324198b3b591dd04eeb6c1850f295f68a10090a
Michael Dürre <michael.duerre@highstreet-technologies.com>
2019-08-02 06:45:49 +0200

    fixed mediator app
    route requests through apigateway
    
    Issue-ID: SDNC-824
    Signed-off-by: Michael Dürre <michael.duerre@highstreet-technologies.com>
    Change-Id: Ief159a71888f271a7751884260f85be01efdd6b4

--- sdnr/wt/odlux/apps/mediatorApp/src/actions/mediatorConfigActions.ts
+++ sdnr/wt/odlux/apps/mediatorApp/src/actions/mediatorConfigActions.ts
@@ -1,3 +1,20 @@
+/**
+ * ============LICENSE_START========================================================================
+ * ONAP : ccsdk feature sdnr wt odlux
+ * =================================================================================================
+ * Copyright (C) 2019 highstreet technologies GmbH Intellectual Property. All rights reserved.
+ * =================================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * ============LICENSE_END==========================================================================
+ */
 
 import { Action } from '../../../../framework/src/flux/action';
 import { Dispatch } from '../../../../framework/src/flux/store';
@@ -12,25 +29,25 @@ import { MediatorConfig, MediatorConfigResponse } from '../models/mediatorServer
 export class BaseAction extends Action { }
 
 export class SetMediatorBusyByName extends BaseAction {
-  constructor (public name: string, public isBusy: boolean) {
+  constructor(public name: string, public isBusy: boolean) {
     super();
   }
 }
 
 export class AddMediatorConfig extends BaseAction {
-  constructor (public mediatorConfig: MediatorConfigResponse) {
+  constructor(public mediatorConfig: MediatorConfigResponse) {
     super();
   }
 }
 
 export class UpdateMediatorConfig extends BaseAction {
-  constructor (public name: string, public mediatorConfig: MediatorConfigResponse) {
+  constructor(public name: string, public mediatorConfig: MediatorConfigResponse) {
     super();
   }
 }
 
 export class RemoveMediatorConfig extends BaseAction {
-  constructor (public name: string) {
+  constructor(public name: string) {
     super();
   }
 }
@@ -38,13 +55,13 @@ export class RemoveMediatorConfig extends BaseAction {
 
 export const startMediatorByNameAsyncActionCreator = (name: string) => (dispatch: Dispatch, getState: () => IApplicationStoreState) => {
   dispatch(new SetMediatorBusyByName(name, true));
-  const { mediator: { mediatorServerState: { url } } } = getState();
-  if (url) {
-    mediatorService.startMediatorByName(url, name).then(msg => {
+  const { mediator: { mediatorServerState: { id } } } = getState();
+  if (id) {
+    mediatorService.startMediatorByName(id, name).then(msg => {
       dispatch(new AddSnackbarNotification({ message: msg + ' ' + name, options: { variant: 'info' } }));
       // since there is no notification, a timeout will be need here
       window.setTimeout(() => {
-        mediatorService.getMediatorServerConfigByName(url, name).then(config => {
+        mediatorService.getMediatorServerConfigByName(id, name).then(config => {
           if (config) {
             dispatch(new UpdateMediatorConfig(name, config));
           } else {
@@ -62,13 +79,13 @@ export const startMediatorByNameAsyncActionCreator = (name: string) => (dispatch
 
 export const stopMediatorByNameAsyncActionCreator = (name: string) => (dispatch: Dispatch, getState: () => IApplicationStoreState) => {
   dispatch(new SetMediatorBusyByName(name, true));
-  const { mediator: { mediatorServerState: { url } } } = getState();
-  if (url) {
-    mediatorService.stopMediatorByName(url, name).then(msg => {
+  const { mediator: { mediatorServerState: { id } } } = getState();
+  if (id) {
+    mediatorService.stopMediatorByName(id, name).then(msg => {
       dispatch(new AddSnackbarNotification({ message: msg + ' ' + name, options: { variant: 'info' } }));
       // since there is no notification, a timeout will be need here
       window.setTimeout(() => {
-        mediatorService.getMediatorServerConfigByName(url, name).then(config => {
+        mediatorService.getMediatorServerConfigByName(id, name).then(config => {
           if (config) {
             dispatch(new UpdateMediatorConfig(name, config));
           } else {
@@ -86,13 +103,13 @@ export const stopMediatorByNameAsyncActionCreator = (name: string) => (dispatch:
 
 export const addMediatorConfigAsyncActionCreator = (config: MediatorConfig) => (dispatch: Dispatch, getState: () => IApplicationStoreState) => {
   const { Name: name } = config;
-  const { mediator: { mediatorServerState: { url } } } = getState();
-  if (url) {
-    mediatorService.createMediatorConfig(url, config).then(msg => {
+  const { mediator: { mediatorServerState: { id } } } = getState();
+  if (id) {
+    mediatorService.createMediatorConfig(id, config).then(msg => {
       dispatch(new AddSnackbarNotification({ message: msg + ' ' + name, options: { variant: 'info' } }));
       // since there is no notification, a timeout will be need here
       window.setTimeout(() => {
-        mediatorService.getMediatorServerConfigByName(url, name).then(config => {
+        mediatorService.getMediatorServerConfigByName(id, name).then(config => {
           if (config) {
             dispatch(new AddMediatorConfig(config));
           } else {
@@ -112,13 +129,13 @@ export const updateMediatorConfigAsyncActionCreator = (config: MediatorConfig) =
 
 export const removeMediatorConfigAsyncActionCreator = (config: MediatorConfig) => (dispatch: Dispatch, getState: () => IApplicationStoreState) => {
   const { Name: name } = config;
-  const { mediator: { mediatorServerState: { url } } } = getState();
-  if (url) {
-    mediatorService.deleteMediatorConfigByName(url, name).then(msg => {
+  const { mediator: { mediatorServerState: { id } } } = getState();
+  if (id) {
+    mediatorService.deleteMediatorConfigByName(id, name).then(msg => {
       dispatch(new AddSnackbarNotification({ message: msg + ' ' + name, options: { variant: 'info' } }));
       // since there is no notification, a timeout will be need here
       window.setTimeout(() => {
-        mediatorService.getMediatorServerConfigByName(url, config.Name).then(config => {
+        mediatorService.getMediatorServerConfigByName(id, config.Name).then(config => {
           if (!config) {
             dispatch(new RemoveMediatorConfig(name));
           } else {
