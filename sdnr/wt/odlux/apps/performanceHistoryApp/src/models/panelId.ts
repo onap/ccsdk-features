77e83cab0a71102241b31ca2b9837a5701272199
herbert <herbert.eiselt@highstreet-technologies.com>
2019-12-11 17:33:15 +0100

    Remove old bundles
    remove old bundle to replace in further step
    
    Issue-ID: SDNC-996
    Signed-off-by: herbert <herbert.eiselt@highstreet-technologies.com>
    Change-Id: I83f53bdf4c118a632932f3af03f26b68a5cea6c4
    Signed-off-by: herbert <herbert.eiselt@highstreet-technologies.com>

--- sdnr/wt/odlux/apps/performanceHistoryApp/src/models/panelId.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Represents PanelIds for the available Expansional panels.
- */
-export type PanelId = null | "PerformanceData" | "ReceiveLevel" | "TransmissionPower" | "AdaptiveModulation" | "Temperature" | "SINR" | "CPD";
