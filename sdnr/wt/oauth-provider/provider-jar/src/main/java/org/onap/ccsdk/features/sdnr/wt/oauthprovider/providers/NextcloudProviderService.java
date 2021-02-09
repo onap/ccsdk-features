d43a4e829a7d22ced8004d753925c8339de61a1b
Michael DÜrre <michael.duerre@highstreet-technologies.com>
2021-02-09 08:14:07 +0100

    add oauth-provider
    new feature for more login security and external logins
    
    Issue-ID: CCSDK-3142
    Signed-off-by: Michael DÜrre <michael.duerre@highstreet-technologies.com>
    Change-Id: I6a564a0d1dca44b69bfea2e692fec1f4315cab83
    Signed-off-by: Michael DÜrre <michael.duerre@highstreet-technologies.com>

---
+++ sdnr/wt/oauth-provider/provider-jar/src/main/java/org/onap/ccsdk/features/sdnr/wt/oauthprovider/providers/NextcloudProviderService.java
@@ -0,0 +1,73 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : ccsdk features
+ * ================================================================================
+ * Copyright (C) 2021 highstreet technologies GmbH Intellectual Property.
+ * All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ *
+ */
+package org.onap.ccsdk.features.sdnr.wt.oauthprovider.providers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import java.util.Map;
+import org.onap.ccsdk.features.sdnr.wt.oauthprovider.data.OAuthProviderConfig;
+import org.onap.ccsdk.features.sdnr.wt.oauthprovider.data.UserTokenPayload;
+
+public class NextcloudProviderService extends AuthService {
+
+    public NextcloudProviderService(OAuthProviderConfig config, String redirectUri, TokenCreator tokenCreator) {
+        super(config, redirectUri, tokenCreator);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected String getTokenVerifierUri() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Map<String, String> getAdditionalTokenVerifierParams() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected ResponseType getResponseType() {
+        // TODO Auto-generated method stub
+        return ResponseType.TOKEN;
+    }
+
+    @Override
+    protected boolean doSeperateRolesRequest() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    protected UserTokenPayload mapAccessToken(String spayload) throws JsonMappingException, JsonProcessingException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected String getLoginUrl(String callbackUrl) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- sdnr/wt/oauth-provider/provider-jar/src/test/java/org/onap/ccsdk/features/sdnr/wt/oauthprovider/test/TestProperty.java
+++ sdnr/wt/oauth-provider/provider-jar/src/test/java/org/onap/ccsdk/features/sdnr/wt/oauthprovider/test/TestProperty.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP : ccsdk features
  * ================================================================================
- * Copyright (C) 2019 highstreet technologies GmbH Intellectual Property.
+ * Copyright (C) 2021 highstreet technologies GmbH Intellectual Property.
  * All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,33 +19,24 @@
  * ============LICENSE_END=========================================================
  *
  */
-package org.onap.ccsdk.features.sdnr.wt.common.util;
+package org.onap.ccsdk.features.sdnr.wt.oauthprovider.test;
 
-import java.net.Inet4Address;
-import java.net.UnknownHostException;
-import java.util.Map;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.onap.ccsdk.features.sdnr.wt.oauthprovider.data.Config;
 
-public class Environment {
+public class TestProperty {
 
-    public static String getVar(String v) {
-        return getVar(v, null);
-    }
-    public static String getVar(String v, String defaultValue) {
-        if (v.equals("$HOSTNAME")) {
-            String s = null;
-            try {
-                s = Inet4Address.getLocalHost().getHostName();
-            } catch (UnknownHostException e) {
-
-            }
-            if (s != null && s.length() > 0)
-                return s;
-        }
-        Map<String, String> env = System.getenv();
-        for (String envName : env.keySet()) {
-            if (envName != null && envName.equals(v))
-                return env.get(envName);
-        }
-        return defaultValue;
+    @Ignore
+    @Test
+    public void test() {
+        String hostname = Config.getProperty("${HOSTNAME}", null);
+        System.out.println(hostname);
+        assertNotNull(hostname);
+        hostname = Config.getProperty("${HOSTNAME2}", null);
+        System.out.println(hostname);
+        assertNull(hostname);
     }
 }
