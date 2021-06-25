06b2e53843909bf78b0f10444573712cd6898f87
m.miernik <m.miernik@samsung.com>
2021-06-25 12:11:06 +0200

    Fix unit test for apigateway
    Fix in TestAaiServlet.java
    
    Issue-ID: SDNC-1579
    Change-Id: I2a3171473f04d30b173eb419a0b9374db543c3f2
    Signed-off-by: m.miernik <m.miernik@samsung.com>

--- sdnr/wt/apigateway/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/apigateway/test/TestAaiServlet.java
+++ sdnr/wt/apigateway/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/apigateway/test/TestAaiServlet.java
@@ -29,7 +29,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.onap.ccsdk.features.sdnr.wt.apigateway.MyProperties;
 import org.onap.ccsdk.features.sdnr.wt.apigateway.test.helper.HelpAaiServlet;
-import org.onap.ccsdk.features.sdnr.wt.apigateway.test.helper.HelpEsServlet;
 import org.onap.ccsdk.features.sdnr.wt.apigateway.test.helper.HelpServletBase;
 
 public class TestAaiServlet extends HelpServletBase {
@@ -64,10 +63,10 @@ public class TestAaiServlet extends HelpServletBase {
 
         // initEsTestWebserver(port);
         properties.load(new ByteArrayInputStream(tmpconfigcontent2.getBytes()));
-        testrequest(HTTPMETHOD_GET, query, HelpEsServlet.RESPONSE_GET, true);
-        testrequest(HTTPMETHOD_POST, query, HelpEsServlet.RESPONSE_POST, true);
-        testrequest(HTTPMETHOD_PUT, query, HelpEsServlet.RESPONSE_PUT, true);
-        testrequest(HTTPMETHOD_DELETE, query, HelpEsServlet.RESPONSE_DELETE, true);
+        testrequest(HTTPMETHOD_GET, query, HelpAaiServlet.RESPONSE_GET, true);
+        testrequest(HTTPMETHOD_POST, query, HelpAaiServlet.RESPONSE_POST, true);
+        testrequest(HTTPMETHOD_PUT, query, HelpAaiServlet.RESPONSE_PUT, true);
+        testrequest(HTTPMETHOD_DELETE, query, HelpAaiServlet.RESPONSE_DELETE, true);
         testrequest(HTTPMETHOD_OPTIONS, query, "", false);
         // stopTestWebserver();
         if (tmpFile.exists())
