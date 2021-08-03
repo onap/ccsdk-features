256e6371e58e173277f1ad002f83fb85e68d0c94
Ravi Pendurty <ravi.pendurty@highstreet-technologies.com>
2021-08-03 15:41:28 +0530

    New devicemanager for OpenROADM version 7.1
    Support for org.openroadm.device yang revision 7.1 and related yangs
    
    Issue-ID: CCSDK-3412
    
    Signed-off-by: Ravi Pendurty <ravi.pendurty@highstreet-technologies.com>
    Change-Id: Ie9f5b42947c725ed2cdc4f8b3c4ad7f6cdfd7b82
    Signed-off-by: Ravi Pendurty <ravi.pendurty@highstreet-technologies.com>

---
+++ sdnr/wt/devicemanager-onap/openroadm71/provider/src/main/java/org/onap/ccsdk/features/sdnr/wt/devicemanager/openroadm71/impl/InitialDeviceAlarmReader.java
@@ -0,0 +1,159 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : ccsdk features
+ * ================================================================================
+ * Copyright (C) 2020 highstreet technologies GmbH Intellectual Property.
+ * All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ *
+ */
+package org.onap.ccsdk.features.sdnr.wt.devicemanager.openroadm71.impl;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.onap.ccsdk.features.sdnr.wt.common.YangHelper;
+import org.onap.ccsdk.features.sdnr.wt.dataprovider.model.DataProvider;
+import org.onap.ccsdk.features.sdnr.wt.devicemanager.service.DeviceManagerServiceProvider;
+import org.onap.ccsdk.features.sdnr.wt.devicemanager.service.FaultService;
+import org.onap.ccsdk.features.sdnr.wt.devicemanager.types.FaultData;
+import org.onap.ccsdk.features.sdnr.wt.netconfnodestateservice.NetconfBindingAccessor;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.alarm.rev200529.ActiveAlarmList;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.alarm.rev200529.Severity;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.alarm.rev200529.active.alarm.list.ActiveAlarms;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.data.provider.rev201110.Faultlog;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.data.provider.rev201110.SeverityType;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Shabnam Sultana
+ *
+ *         Class to read the initial alarms at the time of device registration
+ *
+ **/
+
+public class InitialDeviceAlarmReader {
+    // variables
+    // in fact there is no alarm counter in the alarm obj, we have to do it on our own globally per device
+    private Integer alarmCounter = 1;
+    private static final Logger log = LoggerFactory.getLogger(InitialDeviceAlarmReader.class);
+    private final NetconfBindingAccessor netConfAccesor;
+    private final @NonNull FaultService faultEventListener;
+    private final DataProvider dataProvider;
+    // end of variables
+
+    // constructors
+    public InitialDeviceAlarmReader(NetconfBindingAccessor accessor, DeviceManagerServiceProvider serviceProvider) {
+        this.netConfAccesor = accessor;
+        this.faultEventListener = serviceProvider.getFaultService();
+        this.dataProvider = serviceProvider.getDataProvider();
+    }
+    // end of constructors
+
+    // protected methods
+    // Mapping the alarm data with the fault data
+    protected FaultData writeFaultData() {
+        FaultData faultData = new FaultData();
+        Optional<ActiveAlarmList> list = this.getActiveAlarmList(this.netConfAccesor);
+        if (list.isPresent() && list.get().getActiveAlarms() != null) {
+            Collection<ActiveAlarms> activeAlarms = YangHelper.getCollection(list.get().getActiveAlarms());
+            if (!activeAlarms.isEmpty()) {
+                for (ActiveAlarms activeAlarm : activeAlarms) {
+                    faultData.add(this.netConfAccesor.getNodeId(), this.alarmCounter, activeAlarm.getRaiseTime(),
+                            activeAlarm.getResource().getDevice().getNodeId().getValue(),
+                            activeAlarm.getProbableCause().getCause().getName(),
+                            checkSeverityValue(activeAlarm.getSeverity()));
+                    this.alarmCounter++;
+                }
+                return faultData;
+            }
+        }
+        return faultData;
+    }
+
+    // Write into the FaultLog
+    protected void writeAlarmLog(FaultData faultData) {
+        if (faultData != null) {
+            List<Faultlog> faultLog = faultData.getProblemList();
+            for (Faultlog fe : faultLog) {
+                this.dataProvider.writeFaultLog(fe);
+            }
+        }
+    }
+
+    // Use the FaultService for Alarm notifications
+    protected void faultService() {
+        this.faultEventListener.initCurrentProblemStatus(this.netConfAccesor.getNodeId(), writeFaultData());
+        writeAlarmLog(writeFaultData());
+    }
+    // Mapping Severity of AlarmNotification to SeverityType of FaultLog
+    protected static SeverityType checkSeverityValue(Severity severity) {
+        SeverityType severityType = null;
+        log.info("Device Severity: {}", severity.getName());
+
+        switch (severity.getName()) {
+            case ("warning"):
+                severityType = SeverityType.Warning;
+                break;
+            case ("major"):
+                severityType = SeverityType.Major;
+                break;
+            case ("minor"):
+                severityType = SeverityType.Minor;
+                break;
+            case ("clear"):
+                severityType = SeverityType.NonAlarmed;
+                break;
+            case ("critical"):
+                severityType = SeverityType.Critical;
+                break;
+            case ("indeterminate"):
+                severityType = SeverityType.Critical;
+                break;
+            default:
+                severityType = SeverityType.Critical;
+                break;
+
+        }
+        return severityType;
+
+    }
+    // end of protected methods
+
+    // private methods
+
+    // Read Alarm Data
+    private Optional<ActiveAlarmList> getActiveAlarmList(NetconfBindingAccessor accessor) {
+        final Class<ActiveAlarmList> classAlarm = ActiveAlarmList.class;
+        log.info("Get Alarm data for element {}", accessor.getNodeId().getValue());
+        InstanceIdentifier<ActiveAlarmList> alarmDataIid = InstanceIdentifier.builder(classAlarm).build();
+
+        ActiveAlarmList alarmData = accessor.getTransactionUtils().readData(accessor.getDataBroker(),
+                LogicalDatastoreType.OPERATIONAL, alarmDataIid);
+
+        log.info("AlarmData {}", alarmData);
+        return Optional.ofNullable(alarmData);
+    }
+
+
+    // end of private methods
+
+
+
+}
---
+++ sdnr/wt/devicemanager-onap/openroadm71/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/devicemanager/openroadm/test/TestOpenRoadmDeviceChangeNotification.java
@@ -0,0 +1,120 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : ccsdk features
+ * ================================================================================
+ * Copyright (C) 2020 highstreet technologies GmbH Intellectual Property.
+ * All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ *
+ */
+package org.onap.ccsdk.features.sdnr.wt.devicemanager.openroadm.test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.util.Arrays;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
+import org.junit.Test;
+import org.onap.ccsdk.features.sdnr.wt.dataprovider.model.DataProvider;
+import org.onap.ccsdk.features.sdnr.wt.dataprovider.model.NetconfTimeStamp;
+import org.onap.ccsdk.features.sdnr.wt.dataprovider.model.types.NetconfTimeStampImpl;
+import org.onap.ccsdk.features.sdnr.wt.devicemanager.openroadm71.impl.OpenroadmDeviceChangeNotificationListener;
+import org.onap.ccsdk.features.sdnr.wt.netconfnodestateservice.NetconfAccessor;
+import org.onap.ccsdk.features.sdnr.wt.websocketmanager.model.WebsocketManagerService;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.types.rev200529.RpcStatus;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev200529.ChangeNotification;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev200529.CreateTechInfoNotification;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev200529.CreateTechInfoNotificationBuilder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev200529.change.notification.Edit;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev200529.change.notification.EditBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.netconf.base._1._0.rev110601.EditOperationType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.data.provider.rev201110.EventlogBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.data.provider.rev201110.EventlogEntity;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.data.provider.rev201110.SourceType;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.PathArgument;
+
+
+
+public class TestOpenRoadmDeviceChangeNotification {
+    private static final String NODEID = "Roadm1";
+    private NetconfAccessor netconfAccessor = mock(NetconfAccessor.class);
+    private DataProvider databaseService = mock(DataProvider.class);
+    WebsocketManagerService notificationService = mock(WebsocketManagerService.class);
+    private OpenroadmDeviceChangeNotificationListener deviceChangeListener =
+            new OpenroadmDeviceChangeNotificationListener(netconfAccessor, databaseService, notificationService);
+    private static final NetconfTimeStamp ncTimeConverter = NetconfTimeStampImpl.getConverter();
+
+    @Test
+    public void testOnChangeNotification() {
+
+        when(netconfAccessor.getNodeId()).thenReturn(new NodeId(NODEID));
+        Iterable<? extends PathArgument> pathArguments = Arrays.asList(new PathArgument() {
+
+            @Override
+            public int compareTo(PathArgument arg0) {
+                return 0;
+            }
+
+            @Override
+            public Class<? extends DataObject> getType() {
+                return DataObject.class;
+            }
+        });
+        InstanceIdentifier<?> target = InstanceIdentifier.create(pathArguments);
+
+        deviceChangeListener.onChangeNotification(createNotification(EditOperationType.Create, target));
+        EventlogEntity event =
+                new EventlogBuilder().setNodeId(NODEID).setNewValue(String.valueOf(EditOperationType.Create))
+                        .setObjectId(target.getPathArguments().toString()).setCounter(1)
+                        .setAttributeName(target.getTargetType().getName()).setSourceType(SourceType.Netconf).build();
+        verify(databaseService).writeEventLog(event);
+
+    }
+
+    @Test
+    public void testOnCreateTechInfoNotification() {
+        when(netconfAccessor.getNodeId()).thenReturn(new NodeId(NODEID));
+        deviceChangeListener.onCreateTechInfoNotification(createTechInfoNotification());
+        verify(databaseService).writeEventLog(any(EventlogEntity.class));
+    }
+
+    /**
+     * @param type
+     * @return
+     */
+    private static ChangeNotification createNotification(EditOperationType type, InstanceIdentifier<?> target) {
+        ChangeNotification change = mock(ChangeNotification.class);
+
+        @SuppressWarnings("null")
+        final @NonNull List<Edit> edits = Arrays.asList(new EditBuilder().setOperation(type).setTarget(target).build());
+        when(change.nonnullEdit()).thenReturn(edits);
+        return change;
+    }
+
+    private static CreateTechInfoNotification createTechInfoNotification() {
+        CreateTechInfoNotificationBuilder techInfoNotificationBuilder = new CreateTechInfoNotificationBuilder();
+        techInfoNotificationBuilder.setLogFileName("shjkdjld/EHJkk").setShelfId("dsjhdukdgkzw")
+                .setStatus(RpcStatus.Successful).setStatusMessage("TestSuccessful");
+        return techInfoNotificationBuilder.build();
+
+    }
+
+
+}
