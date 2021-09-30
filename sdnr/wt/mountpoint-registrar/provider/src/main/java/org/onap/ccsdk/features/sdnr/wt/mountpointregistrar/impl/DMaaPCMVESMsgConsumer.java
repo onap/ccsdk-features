f64ed7187c008293ee6c25f2b3930543a20b6573
Rafal Wrzesniak <r.wrzesniak@partner.samsung.com>
2021-09-30 12:08:03 +0200

    Adds handling CM provisioning messages
    Adds config, consumer and changes to run it in MountpointRegistrar
    
    Issue-ID: CCSDK-3464
    Signed-off-by: Rafal Wrzesniak <r.wrzesniak@partner.samsung.com>
    Change-Id: I800db7082dc4a84d73ac9e5dffd90c2f3c46ca82

---
+++ sdnr/wt/mountpoint-registrar/provider/src/main/java/org/onap/ccsdk/features/sdnr/wt/mountpointregistrar/impl/DMaaPCMVESMsgConsumer.java
@@ -0,0 +1,53 @@
+/*
+ * ============LICENSE_START========================================================================
+ * ONAP : ccsdk feature sdnr wt mountpoint-registrar
+ * =================================================================================================
+ * Copyright (C) 2021 Samsung Electronics Intellectual Property. All rights reserved.
+ * =================================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * ============LICENSE_END==========================================================================
+ */
+
+package org.onap.ccsdk.features.sdnr.wt.mountpointregistrar.impl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DMaaPCMVESMsgConsumer extends DMaaPVESMsgConsumerImpl {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DMaaPCMVESMsgConsumer.class);
+
+    public DMaaPCMVESMsgConsumer(GeneralConfig generalConfig) {
+        super(generalConfig);
+        LOG.info("DMaaPCMVESMsgConsumer started successfully");
+    }
+
+    @Override
+    public void processMsg(String msg) throws InvalidMessageException, JsonProcessingException {
+        LOG.debug("Processing CM message {}", msg);
+        JsonNode rootNode = convertMessageToJsonNode(msg);
+        JsonNode dataNode;
+        JsonNode notificationNode;
+        try {
+            dataNode = rootNode.get("event").get("stndDefinedFields").get("data").requireNonNull();
+            if(dataNode.get("notificationType").textValue().equalsIgnoreCase("notifyMOIChanges")) {
+                notificationNode = dataNode.get("moiChanges");
+                LOG.info("Read CM message from DMaaP topic that is moiChanges type with id {}", dataNode.get("notificationId"));
+            }
+        } catch (NullPointerException e) {
+            LOG.warn("Message is invalid, sending aborted, processing stopped because one of fields is missing");
+            throw new InvalidMessageException("Missing field");
+        }
+        // take required data from notificationNode
+    }
+
+}
