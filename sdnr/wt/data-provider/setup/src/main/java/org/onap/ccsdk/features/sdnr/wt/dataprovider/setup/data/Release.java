4bfc7615c273d1bf0a1934768e161d15b10ce1b9
sai-neetha <sai-neetha.phulmali@highstreet-technologies.com>
2023-10-18 11:47:57 +0200

    Support MariaDB Version 11.1.x for SDNRDB
    Support MariaDB Version 11.1.x for SDNRDB
    
    Issue-ID: SDNC-1821
    Change-Id: Ibe56253df5da5672f6d6f02225081af9c9e5a4f4
    Signed-off-by: sai-neetha <sai-neetha.phulmali@highstreet-technologies.com>

--- sdnr/wt/data-provider/setup/src/main/java/org/onap/ccsdk/features/sdnr/wt/dataprovider/setup/data/Release.java
+++ sdnr/wt/data-provider/setup/src/main/java/org/onap/ccsdk/features/sdnr/wt/dataprovider/setup/data/Release.java
@@ -36,7 +36,7 @@ public enum Release {
 	ISTANBUL_R1("istanbul-R1", "-v6", new DatabaseVersion(7,1,1), new DatabaseVersion(8,0,0), false,
             new DatabaseVersion(10,2,7), new DatabaseVersion(10,6,0), false),
     JAKARTA_R1("jakarta-R1", "-v7", new DatabaseVersion(7,1,1), new DatabaseVersion(8,0,0), false,
-            new DatabaseVersion(10,2,7), new DatabaseVersion(10,6,0), false);
+            new DatabaseVersion(10,2,7), new DatabaseVersion(11,1,5), false);
 
 
     public static final Release CURRENT_RELEASE = Release.JAKARTA_R1;
