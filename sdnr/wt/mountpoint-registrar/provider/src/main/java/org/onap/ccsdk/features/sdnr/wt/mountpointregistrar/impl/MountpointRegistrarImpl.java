103be5dee7bd7ae5f4a6fb4863e7dff9651b7dcf
Ravi Pendurty <ravi.pendurty@highstreet-technologies.com>
2024-03-17 13:27:34 +0530

    Kafka Admin client to use authentication properties
    Supports SCRAM-SHA-512
    
    Issue-ID: CCSDK-3998
    Change-Id: I7c03d62a596e3a34834dec7933ce4192db48c952
    Signed-off-by: Ravi Pendurty <ravi.pendurty@highstreet-technologies.com>

--- sdnr/wt/mountpoint-registrar/provider/src/main/java/org/onap/ccsdk/features/sdnr/wt/mountpointregistrar/impl/MountpointRegistrarImpl.java
+++ sdnr/wt/mountpoint-registrar/provider/src/main/java/org/onap/ccsdk/features/sdnr/wt/mountpointregistrar/impl/MountpointRegistrarImpl.java
@@ -48,7 +48,7 @@ public class MountpointRegistrarImpl implements AutoCloseable, IConfigChangedLis
     private StrimziKafkaVESMsgConsumerMain sKafkaConsumerMain = null;
     private StrimziKafkaConfig strimziKafkaConfig;
 
-    // Blueprint 1
+    // Blueprint 1 
     public MountpointRegistrarImpl() {
         LOG.info("Creating provider class for {}", APPLICATION_NAME);
     }
@@ -95,7 +95,11 @@ public class MountpointRegistrarImpl implements AutoCloseable, IConfigChangedLis
     @Override
     public void onConfigChanged() {
         if (generalConfig == null) { // Included as NullPointerException observed once in docker logs
-            LOG.warn("onConfigChange cannot be handled. Unexpected Null");
+            LOG.warn("onConfigChange cannot be handled. Unexpected Null for generalConfig");
+            return;
+        }
+        if (strimziKafkaConfig == null) { // Included as NullPointerException observed once in docker logs
+            LOG.warn("onConfigChange cannot be handled. Unexpected Null for strimziKafkaConfig");
             return;
         }
         LOG.info("Service configuration state changed. Enabled: {}", strimziKafkaConfig.getEnabled());
